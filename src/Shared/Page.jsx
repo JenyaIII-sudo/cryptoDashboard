d2cb1f21c29b1b8df86cb6ac8495d4a6cad5c1d4
John Denver <denverDevelop@gmail.com>
2020-02-17 12:27:32 +0300

    Added Page component with  propTypes

---
+++ src/Shared/Page.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react';
+import PropTypes from 'prop-types';
+import { AppContext } from '../App/AppProvider';
+
+export default function Page({ name, children }) {
+  const context = useContext(AppContext);
+  const { page } = context;
+  if (page !== name) {
+    return null;
+  }
+  return <div>{children}</div>;
+}
+
+Page.defaultProps = {
+  children: [],
+  name: '',
+};
+
+Page.propTypes = {
+  name: PropTypes.string,
+  children: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string])),
+};
